989cc71c07afd3cd33bafd2d96953d0ec9251025
BybYb <2519878+bybybybyb@users.noreply.github.com>
2022-09-02 02:26:01 -0400

    partially worked not ideal svg inline

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
+import { svgLoader } from "./svg";
 import vue from "@vitejs/plugin-vue2";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteCompression()],
+  plugins: [vue(), viteCompression(), svgLoader()],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
