6d3a52ce270931ae611a4321d2bc90d350d2c819
BybYb <2519878+bybybybyb@users.noreply.github.com>
2022-09-02 00:52:03 -0400

    gzip build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 import vue from "@vitejs/plugin-vue2";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteCompression()],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
